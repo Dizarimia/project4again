db7592d888ee331de6cf9418a116fe089511d75e
Dizarimia <lillemor.sunnegard@outlook.com>
2022-11-25 18:57:46 +0000

    add to gitignore

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +76,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse('postgres://kfptugmaupjvze:2e717558a1eedfd2e24862f947bc253a0e22f46674caaf64136282426becc691@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/delco4facur0tr')
 }
+
 
 
 # Password validation
